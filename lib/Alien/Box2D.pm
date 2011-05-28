f44b2768215c55a546e5b6e72edcfe5aa6ffad8d
Tobias Leich <email@froggs.de>
2011-05-28 22:03:56 +0200

    version is now 0.102, logged changes, using Alien::CMake 0.03

--- lib/Alien/Box2D.pm
+++ lib/Alien/Box2D.pm
@@ -13,11 +13,11 @@ Alien::Box2D - Build and make available Box2D library - L<http://box2d.org/>
 
 =head1 VERSION
 
-Version 0.101
+Version 0.102
 
 =cut
 
-our $VERSION = '0.101';
+our $VERSION = '0.102';
 $VERSION = eval $VERSION;
 
 =head1 SYNOPSIS
